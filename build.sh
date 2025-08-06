36eb0353febbeabcd1165dc2d74c542a3c519abf
M-Chero <mitchiemonroe43@gmail.com>
2025-08-06 21:26:17 +0300

    Deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
-#!/usr/bin/env bash
-set -o errexit
+# #!/usr/bin/env bash
+# set -o errexit
 
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+
+# python manage.py migrate
+
+
+#!/bin/bash
+
+echo "Upgrading pip, setuptools, and wheel..."
+pip install --upgrade pip setuptools wheel
+
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
