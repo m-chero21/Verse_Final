82b4c6970676a47cb4105f491db306851610d6ee
M-Chero <mitchiemonroe43@gmail.com>
2025-08-07 06:55:32 +0300

    Deployment5

--- myproject/settings.py
+++ myproject/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-...'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # ======================== INSTALLED APPS ========================
